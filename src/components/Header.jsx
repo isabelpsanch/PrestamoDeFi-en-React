dec47e4fe770f73e2f56ae882f5ddb86c981c32b
isabelpsanch <isabelparraehs@gmail.com>
2024-03-20 14:50:53 +0100

    Añadidos nuevos cambios y dibujos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ export default function Header() {
     return (
       <header className="py-2 px-3 sm:py-4 sm:px-8 flex justify-between items-center bg-white border-b shadow-xs">
         {/* Logo para version mobile */}
-        <img src="/cropped-favicon-192x192.npg" alt="blockmaker-logo" width={47} className="sm:hidden" />
+        <img src="Logo-DelFi-CotractInteligente.png" alt="PrestamoDeFi-Logo" width={47} className="sm:hidden" />
         {/* Logo para version desktop */}
-        <img src="/blockmaker-full-logo(1).png" alt="blockmaker-logo" width={300} className="hidden sm:flex" />
+        <img src="/Logo-DeFi-Contract-Inteligent-Big.png" alt="PrestamoDeFi-Logo" width={300} className="hidden sm:flex" />
       </header>
     )
   }
